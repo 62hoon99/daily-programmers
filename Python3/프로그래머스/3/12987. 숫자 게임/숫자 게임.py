6b4745d74d8a17dad1207c7dd6e895d079fe3787
유기훈 <98267772+62hoon99@users.noreply.github.com>
2026-04-04 09:16:06 +0900

    [level 3] Title: 숫자 게임, Time: 45.42 ms, Memory: 17.4 MB -BaekjoonHub

--- Python3/프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ Python3/프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,19 +1,12 @@
-from bisect import bisect_right, insort
-
 def solution(A, B):
     answer = 0
-    b_sorted = sorted(B)  # 중복 포함 정렬 리스트
+    A.sort()
+    B.sort()
+    j = 0
 
-    for a in A:
-        # a보다 큰 값 중 가장 작은 것의 index 탐색
-        idx = bisect_right(b_sorted, a)
-
-        if idx < len(b_sorted):
-            # 이길 수 있는 카드가 있으면 사용
-            answer += 1
-            del b_sorted[idx]
-        else:
-            # 이길 수 없으면 가장 작은 카드를 희생
-            del b_sorted[0]
+    for i in range(len(A)):
+        if A[j] < B[i]:
+            answer = answer + 1
+            j = j+1
 
     return answer

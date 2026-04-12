ad62c92a7781e1744cd19732a491568c8b777615
유기훈 <98267772+62hoon99@users.noreply.github.com>
2026-04-12 09:42:01 +0900

    [level 2] Title: 요격 시스템, Time: 381.42 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -0,0 +1,13 @@
+def solution(targets):
+    answer = 0
+    
+    targets.sort(key=lambda x: x[1])
+    
+    end = -1
+    
+    for s, e in targets:
+        if s >= end:
+            answer += 1
+            end = e
+    
+    return answer
